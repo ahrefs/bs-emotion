209a0bba744282b4a86dc2e4ef3a8e4b3f0fdc79
Alex Fedoseev <alex.fedoseev@gmail.com>
2018-11-19 10:04:09 +0800

    Support match in ppx

--- bs-emotion-ppx/test/Test.ml
+++ bs-emotion-ppx/test/Test.ml
@@ -17,3 +17,19 @@ let baz ~x ~y = [%css [
 ]]
 
 let empty = [%css []]
+
+type t = A | B
+
+let fn1 x = [%css (
+  match x with
+  | A -> [ prop "a"; ]
+  | B -> [ prop "b"; ]
+)]
+
+let fn2 x y = [%css (
+  match x with
+  | A when y -> [ prop "ay"; ]
+  | A -> [ prop "a"; ]
+  | B when y -> [ prop "by"; ]
+  | B -> [ prop "b"; ]
+)]
